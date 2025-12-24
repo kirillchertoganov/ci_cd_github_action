4efe8ab2a006eaa120e04c6cfa9f12e1a0576174
Kirill <kirill@yandex.com>
2025-12-24 08:31:43 +0300

    feat: add hello world docker container

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require("http");
+
+http.createServer((req, res) => {
+  res.end("Hello from CI/CD 🚀");
+}).listen(3000, "0.0.0.0", () => {
+  console.log("Server running on port 3000");
+});
+
